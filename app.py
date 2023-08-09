f9319b7132d9807edba2d4248f0ea2da5d0e54c9
Anirudh Vadlamani <anirudhvadlamani2001@gmail.com>
2023-08-09 08:33:53 +0400

    add questions and answers route

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from flask_debugtoolbar import DebugToolbarExtension
-from surveys import surveys
+from surveys import surveys, satisfaction_survey
 
 app = Flask(__name__)
 
@@ -19,3 +19,20 @@ def home():
     title = surveys['satisfaction'].title
     instructions = surveys['satisfaction'].instructions
     return render_template('home.html', title=title, instructions=instructions)
+
+
+@app.route('/questions/<int:id>')
+def question_one(id):
+    question = satisfaction_survey.questions[id].question
+    choices = satisfaction_survey.questions[id].choices
+    return render_template('questions.html', question=question, choices=choices)
+
+
+@app.route('/answers', methods=['POST'])
+def get_answers():
+    answer = request.args.get('answer')
+    responses.append(answer)
+    if len(responses) == len(satisfaction_survey.questions):
+        return render_template('thank-you.html')
+    else:
+        return redirect(f'/questions/{len(responses)}')
